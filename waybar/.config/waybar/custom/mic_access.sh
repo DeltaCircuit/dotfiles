69fae104ba267b78347c4b3e72f1c0c5fee830c5
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2025-06-19 00:58:47 +0530

    custom mic access

---
+++ waybar/.config/waybar/custom/mic_access.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+
+readarray -t media_names < <(pactl -f json list source-outputs | jq -r '.[].properties["media.name"]')
+
+apps=${#media_names[@]}
+
+if [ $apps -gt 0 ]
+then
+    tooltip=""
+    for item in "${media_names[@]}"; do
+        tooltip+=" $item"$'\r'
+    done      
+    echo "{\"text\": \" 󰻍 \", \"tooltip\": \"$tooltip\" }"
+else
+    echo '{}'
+fi
+
+
