99e7b6f9c6f8ccf5dfe81b62aa9c664728fafd61
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2025-08-07 09:00:50 +0530

    feat: buffer keymaps

--- nvim/.config/nvim/lua/kuttichathan/core/keymaps.lua
+++ nvim/.config/nvim/lua/kuttichathan/core/keymaps.lua
@@ -9,3 +9,9 @@ local opts = { noremap = true, silent = true }
 
 -- Save File  
 set('n', '<C-s>', '<cmd> w <CR>', opts)
+
+-- Buffer Management
+vim.keymap.set('n', '<C-Tab>', ':bnext<CR>', opts)
+vim.keymap.set('n', '<C-Shift-Tab>', ':bprevious<CR>', opts)
+vim.keymap.set('n', '<C-w>', ':bdelete!<CR>', opts)   -- close buffer
+vim.keymap.set('n', '<C-n>', '<cmd> enew <CR>', opts) -- new buffer
