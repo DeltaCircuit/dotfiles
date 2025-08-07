d07297dc6303c5f8062cd3c192793415a8f4d159
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2025-08-07 11:32:33 +0530

    neotree current buffer on

--- nvim/.config/nvim/lua/kuttichathan/lazy/neo-tree.lua
+++ nvim/.config/nvim/lua/kuttichathan/lazy/neo-tree.lua
@@ -27,7 +27,12 @@ return {
 			end,
 			keys = {
 				{ "<leader>w", ":Neotree toggle float<CR>", silent = true, desc = "Float File Explorer" },
-				{ "<leader>e", ":Neotree toggle position=left<CR>", silent = true, desc = "Left File Explorer" },
+				{
+					"<leader>e",
+					":Neotree toggle position=left reveal=true<CR>",
+					silent = true,
+					desc = "Left File Explorer",
+				},
 				{
 					"<leader>ngs",
 					":Neotree float git_status<CR>",
